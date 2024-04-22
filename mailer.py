b87dbe203f084891f157c7c846f245776a9f31c4
tt6578 <167436360+tt6578@users.noreply.github.com>
2024-04-21 22:17:33 -0700

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -12,12 +12,12 @@ load_dotenv()
 
 # Import those variables from .env file
 
-email_sender = os.getenv("email")
-email_password = os.getenv("password")
+email_sender = os.getenv("invoice@ec-japan.jp")
+email_password = os.getenv("supporthelpdesk!123")
 
 # Tested with a temporary email
 
-email_receiver = 'hakele3350@laserlip.com'
+email_receiver = 'wilddivarider@yahoo.com'
 
 subject = "I'm a python mailer, I work"
 body = """
@@ -27,13 +27,14 @@ Hope you're good engineer?
 """
 
 em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['subject'] = subject
-em.set_content(body)
+em['From'] = invoice@ec-japan.jp
+em['To'] = wilddivarider@yahoo.com
+em['subject'] = "I'm a python mailer, I work"
+body 
+em.set_content(Hope you're good engineer?)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender, email_password)
+with smtplib.SMTP_SSL('smtp.office365.com', 587, context=context) as smtp:
+    smtp.login(invoice@ec-japan.jp, supporthelpdesk!123)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
